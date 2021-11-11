6ec02715d71718e2b6acdab4a159091642df64f3
ilja115610 <57315435+ilja115610@users.noreply.github.com>
2021-11-11 18:26:17 +0200

    docker compose added

--- Andevis-backend/src/main/java/com/exchange/andevisbackend/config/UserDetailsServiceImpl.java
+++ Andevis-backend/src/main/java/com/exchange/andevisbackend/config/UserDetailsServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserAccountRepository userRepository;
+
     @Autowired
     public UserDetailsServiceImpl(UserAccountRepository userRepository) {
         this.userRepository = userRepository;
--- Andevis-backend/src/main/java/com/exchange/andevisbackend/config/WebConfig.java
+++ Andevis-backend/src/main/java/com/exchange/andevisbackend/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost")
                 .exposedHeaders("Authorization");
     }
 }
